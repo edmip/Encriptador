eabb520ed4656f155fe9146296d84d3f87dd260a
edmip <edmiasdel@gmail.com>
2024-07-27 16:00:06 -0500

    agregando modo oscuro y mejoras en el codigo de los mensajes de error

--- index.js
+++ index.js
@@ -18,18 +18,6 @@ let areaEncriptar = document.querySelector(".contenedor__texto__encriptar");
 let areaResultado = document.querySelector(".contenedor__texto__desencriptar");
 
 
-//fumciom para poner o quitar visibilidad de campos
-function escritura(){
-   
-    if (areaResultado.trim !==""){
-        divDesencriptarLetras.style.visibility = "hidden";
-        divDesencriptarbotones.style.visibility = "visible";
-    }
-       
-
-}
-
-
 //FUNCION PARA LIMPIAR EN EL CAMPO DEL RESULTADO
 function colocarResultado(texto){
     areaResultado.value = texto;
@@ -81,20 +69,10 @@ function encriptarTexto(){
         
         }
 
-        //LLAMAMOS A LA FUNCION PARA COLOCAL EL TEXTO EN EL AREA DE RESULTADO
-        colocarResultado(texto);
+    //LLAMAMOS A LA FUNCION PARA COLOCAL EL TEXTO EN EL AREA DE RESULTADO
+    colocarResultado(texto);
 }else{
-    
-    Swal.fire({
-        title: "¡Ups! Algo salio mal",
-        text: "INTENTA LO SIGUIENTE: \n ✦ Solo Letras Minusculas \n ✦ No Numeros ✦ No Caracteres especiales o acentos ✦",
-        showConfirmButton: false,
-        timerProgressBar: true,
-        timer: 4000,
-        background:  '#F7B0A8',
-      });
-
-      
+    MensajeError(textoParaEncriptar);   
 }
 //areaEncriptar.focus();
 }
@@ -108,6 +86,7 @@ function desencriptarTexto() {
     let textoDesencriptado = textoParaDesencriptar;
 
     if(regex.test(textoDesencriptado) && textoDesencriptado.length > 0){
+
         for (let i = 0; i < arraPalabras.length; i++) {
 
             if (textoParaDesencriptar.includes(arraPalabras[i])) {
@@ -119,15 +98,7 @@ function desencriptarTexto() {
         //LLAMAMOS A LA FUNCION PARA COLOCAL EL TEXTO EN EL AREA DE RESULTADO
         colocarResultado(textoDesencriptado)
     }else{
-                    
-            Swal.fire({
-                title: "¡Ups! Algo salio mal",
-                text: "INTENTA LO SIGUIENTE: \n ✦ Solo Letras Minusculas \n ✦ No Numeros ✦ No Caracteres especiales o acentos",
-                showConfirmButton: false,
-                timer: 4000,
-                timerProgressBar: true,
-                background:  '#F7B0A8',
-              });
+        MensajeError(textoDesencriptado);
     }
 }
 
@@ -136,20 +107,26 @@ function desencriptarTexto() {
 function copiarTexto(){
 
     textoCopiado=areaResultado.value;
-    navigator.clipboard.writeText(textoCopiado);
 
-  
-      const Toast = Swal.mixin({
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 1000,
-        background:  '#C6F7A8',
-      });
-      Toast.fire({
-        icon: "success",
-        title: "Texto Copiado.!"
-      });
+    if(textoCopiado.length > 0 && regex.test(textoCopiado)){
+
+        navigator.clipboard.writeText(textoCopiado);
+
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 1000,
+                background:  '#C6F7A8',
+            });
+            Toast.fire({
+                icon: "success",
+                title: "Texto Copiado.!"
+            });
+   }else{
+    MensajeError(textoCopiado);
+   }
+    
 }
 
 //FUINCION DE LIMPIAR EL AREA DE TEXTO DE IZQUIERDA 
@@ -157,6 +134,60 @@ function limpiarTexto(){
 
     areaEncriptar.value = "";
 }
+
+//FUINCION cambiar de color
+
+let checkbox= document.getElementById("inputCheck");
+
+function cambiaDeColor (){
+
+        if(checkbox.checked){
+         document.documentElement.style.setProperty('--color-primario', '#86868B'); //BOTONES
+         document.documentElement.style.setProperty('--color-secundario', '#131313'); //FONDO BODY
+         document.documentElement.style.setProperty('--color-terciario', '#2B2B2B'); //TEXTAREA
+         document.documentElement.style.setProperty('--color-hover', '#84A7A1');  // OVER BOTONES
+         document.documentElement.style.setProperty('filter: saturate', '3');
+        }
+        else{   
+        document.documentElement.style.setProperty('--color-primario', '#052051'); 
+        document.documentElement.style.setProperty('--color-secundario', '#E9ECF8'); 
+        document.documentElement.style.setProperty('--color-terciario', '#FFFFFF'); 
+        document.documentElement.style.setProperty('--color-hover', '#0A3871');  
+        }
+        
+
+}
+
+// FUNCION PARA ARROJAR MENSAJE DE ERROR DEPENDIENDO DE LO QUE ESTE EN EL CAMPO DE TEXTO
+function MensajeError(texto){
+
+    
+
+    if (texto.length <= 0){
+        
+        Swal.fire({
+            title: "¡Campo Vacio!",
+            text: "✦ Ingresa Texto ✦",
+            showConfirmButton: false,
+            timer: 2000,
+            timerProgressBar: true,
+            background:  '#F7B0A8',
+          });
+          return true;
+
+    }else {
+        Swal.fire({
+            title: "¡Ups! Algo salio mal",
+            text: "INTENTA LO SIGUIENTE: \n ✦ Solo Letras Minusculas \n ✦ No Numeros ✦ No Caracteres especiales o acentos",
+            showConfirmButton: false,
+            timer: 4000,
+            timerProgressBar: true,
+            background:  '#F7B0A8',
+          });
+          return true;
+    }
+}
+
 
 
         
